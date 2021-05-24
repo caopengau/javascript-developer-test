a33e1344abcb2284a10b7bc3d119e07f01fb5a37
Cao Peng <caopengau@gmail.com>
2021-05-25 01:59:44 +1000

    add solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // convert url to fetch promises
+  const fetchPromises = urls.map((url) => {
+    return httpGet(url);
+  });
+  // await all promises in parallel
+  const fetchResults = await Promise.all(fetchPromises);
+
+  // requirement logic 
+  const results = fetchResults.map((fetchResult) => {
+    // message will be extracted regardless of the fetch result status
+    const message = JSON.parse(fetchResult.body).message;
+    return fetchResult.status === 200 // status will determine the key
+      ? { "Arnie Quote": message }
+      : { FAILURE: message };
+  });
+  return results;
 };
 
 module.exports = {
